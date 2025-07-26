298d354f8ff439586ea682f8a4d5f6a837095a78
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-07-26 01:16:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,8 @@
+// src/lib/firebase-config.ts
+export const firebaseConfig = {
+  projectId: "content-forge-xnfzf",
+  appId: "1:524735337008:web:02a97cd03b0468f569d3f5",
+  storageBucket: "content-forge-xnfzf.firebasestorage.app",
+  apiKey: "AIzaSyCptpuyJ_xbK6aacUXN3mCxz5r7KYq7c2s",
+  authDomain: "content-forge-xnfzf.firebaseapp.com",
+};
