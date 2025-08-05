533371b7b67385f38906c624314a2adc63af460e
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-08-05 23:21:27 +0000

    Description Internal URLs with a client-side error. This indicates a pro

--- src/next.config.ts
+++ src/next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
